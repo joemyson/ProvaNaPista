20d121c8fd4644d44be05f0fd313212392da588d
joemyson <joemyson1402@gmail.com>
2021-12-21 16:38:55 -0300

    criação da pasta SRC e Entities(class BaseProduct e UserProduct)

---
+++ Api.Domain/Entities/BaseProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public abstract class BaseProduct
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createAt;
+
+        private DateTime? CreateAt
+        {
+            get { return _createAt; }
+    set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
+
+        public DateTime UpDateAt { get; set; }
+
+
+    }
+}
---
+++ Api.Domain/Entities/UserProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entities
+{
+    public class UserProduct : BaseProduct
+    {
+
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public int Stock { get; set; }
+    }
+}
